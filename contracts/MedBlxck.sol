32304701a8f401dc79919eb62cc3c8f7ab24e2ac
Luqxus <137922070+Luqxus@users.noreply.github.com>
2024-05-03 14:15:58 +0000

    patient && doctor && access => struct || DoctorType enum

--- contracts/MedBlxck.sol
+++ contracts/MedBlxck.sol
@@ -1,4 +1,32 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-contract MedBlxck {}
+contract MedBlxck {
+    struct Patient {
+        address patientID;
+    }
+    struct Record {
+        string recordID;
+        address doctorID;
+        address patientID;
+        Access[] access;
+    }
+    struct Doctor {
+        address doctorID;
+        uint256 lisenceNumber;
+        DoctorType doctorType;
+    }
+    struct Access {
+        address owner;
+        address receipent;
+    }
+
+    enum DoctorType {
+        PEDIATRICIAN,
+        DERMATOLOGIST,
+        EPIDEMIOLOGIST,
+        GYNECOLOGIST,
+        SURGEON,
+        GERIATRICIAN
+    }
+}
